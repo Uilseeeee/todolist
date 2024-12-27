49f111ff144d794eb157f74daf2e7703b5bfe4b8
Uilsee <uilseeu437@gmail.com>
2024-12-27 09:31:50 +0800

    main

--- todo.js
+++ todo.js
@@ -1,3 +1,17 @@
-Document.getElementsByClassName('add-text').addEventListener('click', function () {
-    Document.getElementById('todo').style.display = 'block';
-});
+// Document.getElementsByClassName('add-text').addEventListener('click', function () {
+//     Document.getElementById('todo').style.display = 'block';
+// });
+
+const modalContainerEl = document.getElementById("modal")
+const optionsEl = document.getElementById("test")
+
+const addTask = () => {
+    console.log("ehellee");
+    
+  modalContainerEl.appendChild(optionsEl);
+  optionsEl.style.display = "block"
+  optionsEl.style.background = "red"
+
+  console.log("duuslaa");
+  
+}
